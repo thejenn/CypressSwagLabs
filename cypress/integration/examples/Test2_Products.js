d1c7b0d0c4c507c58e938d52134e83b71feec341
jennharmless <jharmless@riis.com>
2023-05-16 10:32:56 -0400

    Beginning product page tests

--- cypress/integration/examples/Test2_Products.js
+++ cypress/integration/examples/Test2_Products.js
@@ -1,7 +1,35 @@
 /// <reference types="cypress" />
 
-describe('Products', function() {
-    it('Add Backpack to cart', function() {
+describe('Products Page', function() {
+    it('Add Backpack to Cart', function() {
+
+
+        cy.visit("https://www.saucedemo.com/")
+        cy.get('#user-name').type('standard_user')
+        cy.get("#password").type('secret_sauce')
+        cy.get('#login-button').click()
+
+        cy.get('.inventory_item_description').each(($el, index, $list) => {
+            if ($el.text().includes('Backpack'))
+            {
+                cy.get('#add-to-cart-sauce-labs-backpack').eq(index).click()
+            }  
+            
+       })
+
 
     })
+
 })
+
+
+        
+
+
+
+         
+
+
+  
+
+
